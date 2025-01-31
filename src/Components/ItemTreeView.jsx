821acee11d2ca5b8e0342f27a94e3daee7a5f654
Rhys Lee <78373339+rhyslee211@users.noreply.github.com>
2025-01-31 18:42:55 -0500

    added map moving and zoom functionality

--- src/Components/ItemTreeView.jsx
+++ src/Components/ItemTreeView.jsx
@@ -2,7 +2,7 @@ import { React } from 'react';
 
 function ItemTreeView() {
     return (
-        <div className="h-full w-full bg-white flex items-center justify-center bg-white border-r border-gray-200 rounded-lg w-1/5 pointer-events-auto">
+        <div className="h-full w-1/5 bg-white flex items-center justify-center bg-white border-r border-gray-200 rounded-lg w-1/5 pointer-events-auto">
             The tree view
         </div>
     );
